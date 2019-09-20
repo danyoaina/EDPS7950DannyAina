b07e1cefb5b0848bb431ecc3a23d31be07b1828e
Danny Aina <54292780+danyoaina@users.noreply.github.com>
2019-09-20 04:17:21 -0600

    Add files via upload

---
+++ EDPS7950 WEEK 3/js/main.js
@@ -0,0 +1,13 @@
+// TO DO: Awesome code to handle interactions goes here...
+var cont_sidebar = document.getElementById("cont_sidebar");
+var btn_settings = document.getElementById("btn_settings");
+
+console.log(cont_sidebar);
+console.log(btn_settings);
+
+btn_settings.addEventListener('click',function(){
+    cont_sidebar.classList.toggle("active");
+});
+cont_sidebar.addEventListener('dblclick',function(){
+    cont_sidebar.classList.toggle("active");
+});
